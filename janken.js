825f80f5781078c95d338cc5660a7b37a0b264b3
haruichiu <riob20haruichi@gmail.com>
2023-07-24 21:50:16 +0900

    complete janken

--- janken.js
+++ janken.js
@@ -1,21 +1,61 @@
+function janken() {
+    const gu = 1;
+    const choki = 2;
+    const pa = 3;
 
-var gu = 1;
-var choki = 2;
-var pa = 3;
-
-function humanhand() {
-    var hum = prompt('半角で１～３の数字を入力');
-    hum = parseInt(hum, 10);
-    if (hum != gu && hum != choki && hum != pa) {
-        return undefined;
-    } else {
-        return hum;
+    function humanhand() {
+        var human = prompt('半角で１～３の数字を入力してください。\n\n' + gu + '：グー\n' + choki + '：チョキ\n' + pa + '：パー');
+        human = parseInt(human, 10);
+        if (human != gu && human != choki && human != pa) {
+            return undefined;
+        } else {
+            return human;
+        }
     }
+
+    function computerhand() {
+        return Math.floor(Math.random() * 3) + 1;
+    }
+
+    function handname(number) {
+        switch (number) {
+            case gu:
+                return 'グー';
+            case choki:
+                return 'チョキ';
+            case pa:
+                return 'パー';
+        }
+    }
+
+    function result(computer, human) {
+        if (human === computer) {
+            return 'あいこです.';
+        } else if ((computer === gu && human === pa) || (computer === choki && human === gu) || (computer === pa && human === choki)) {
+            return '勝ちました.';
+        } else {
+            return '負けました.';
+        }
+    }
+
+    function resulttext(computer, human) {
+        return result(computer, human) + 'コンピュータの手は「' + handname(computer) + '」でした';
+    }
+
+    var human = humanhand();
+    if (!human) {
+        alert('入力値を認識できませんでした。ブラウザを再読み込みしてください。');
+    } else {
+        var computer = computerhand();
+        alert(resulttext(computer, human));
+    }
+
+
 }
 
-var button = document.getElementById('button');
 
-button.addEventListener('click', humanhand)
+var button = document.getElementById('button');
+button.addEventListener('click', janken)
 
 
 
